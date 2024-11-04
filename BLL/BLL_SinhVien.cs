70cf4544ab76e59715f5c7709ff9ffe96c4bfa24
Blee123-22DTHG1 <huynhthanhthien4327@gmail.com>
2024-11-04 21:21:02 +0700

    Add files via upload

---
+++ BLL/BLL_SinhVien.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLySinhVien.DAL;
+using QuanLySinhVien.DTO;
+
+namespace QuanLySinhVien.BLL
+{
+    internal class BLL_SinhVien
+    {
+        DAL_SinhVien dalSV = new DAL_SinhVien();
+        public DataTable loadSV()
+        {
+            return dalSV.loadSinhVien();
+        }
+        public DataTable getData(string sql)
+        {
+            return dalSV.getData(sql);
+        }
+        public bool themSV(DTO_SinhVien sv)
+        {
+            return dalSV.themSinhVien(sv);
+        }
+        public bool suaSV(DTO_SinhVien sv)
+        {
+            return dalSV.suaSinhVien(sv);
+        }
+        public bool xoaSV(string maSV)
+        {
+            return dalSV.xoaSinhVien(maSV);
+        }
+        public DataTable timSV(string keyword)
+        {
+            string sql = $"SELECT * FROM SinhVien WHERE MaSV LIKE '%{keyword}%' OR TenSV LIKE '%{keyword}%'";
+            return dalSV.getData(sql);
+        }
+    }
+}
